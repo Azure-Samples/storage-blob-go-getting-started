e76b5afcba3c1b3747504e0fcfb4e828be635282
mcardosos <mcardoso@microsoft.com>
2017-05-03 17:33:33 -0700

    Working with v10

--- storageExample.go
+++ storageExample.go
@@ -3,92 +3,131 @@ package main
 
 import (
 	"encoding/base64"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"os"
 
+	"bytes"
+
 	"github.com/Azure/azure-sdk-for-go/storage"
 )
 
-type storageEnvironment string
+var (
+	accountName string
+	accountKey  string
+	blobCli     storage.BlobStorageClient
+	emulator    *bool
 
-const (
-	emulator storageEnvironment = "emulator"
-	account  storageEnvironment = "account"
+	appendBlobFile = "appendBlob.txt"
+	blockBlobFile  = "blockBlob.txt"
+	pageBlobFile   = "pageBlob.txt"
 )
+
+func init() {
+	emulator = flag.Bool("emulator", false, "use the Azure Storage Emulator")
+	flag.Parse()
+	if *emulator {
+		accountName = storage.StorageEmulatorAccountName
+		accountKey = storage.StorageEmulatorAccountKey
+	} else {
+		accountName = getEnvVarOrExit("ACCOUNT_NAME")
+		accountKey = getEnvVarOrExit("ACCOUNT_KEY")
+	}
+	client, err := storage.NewBasicClient(accountName, accountKey)
+	onErrorFail(err, "Create client failed")
+
+	blobCli = client.GetBlobService()
+
+	// delete all container in the emulator
+	if *emulator {
+		list, err := blobCli.ListContainers(storage.ListContainersParameters{})
+		onErrorFail(err, "List containers failed")
+
+		for _, c := range list.Containers {
+			cnt := blobCli.GetContainerReference(c.Name)
+			err = cnt.Delete(nil)
+			onErrorFail(err, "Delete container failed")
+		}
+	}
+}
 
 func main() {
 	fmt.Println("Azure Storage Blob Sample")
-	err := blobSamples("demoblobconatiner", "demoPageBlob", "demoAppendBlob", "demoBlockBlob")
-	printError(err)
+	blobSamples("demoblobconatiner", "demoPageBlob", "demoAppendBlob", "demoBlockBlob")
 }
 
 // blobSamples creates a container, and performs operations with page blobs, append blobs and block blobs.
-func blobSamples(containerName, pageBlobName, appendBlobName, blockBlobName string) error {
-	fmt.Println("Get credentials...")
-	credentials, err := getCredentials(emulator)
-	// credentials, err := getCredentials(account)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("Create storage client...")
-	client, err := storage.NewBasicClient(credentials["AZURE_STORAGE_ACCOUNT_NAME"], credentials["AZURE_STORAGE_ACCOUNT_KEY"])
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("Create blob client...")
-	blobClient := client.GetBlobService()
-
+func blobSamples(containerName, pageBlobName, appendBlobName, blockBlobName string) {
 	fmt.Println("Create container with private access type...")
-	if _, err := blobClient.CreateContainerIfNotExists(containerName, storage.ContainerAccessTypePrivate); err != nil {
-		if credentials["AZURE_STORAGE_ACCOUNT_NAME"] == storage.StorageEmulatorAccountName {
-			return fmt.Errorf("If you are running with the emulator credentials, plaase make sure you have started the storage emmulator. Press the Windows key and type Azure Storage to select and run it from the list of applications - then restart the sample. %v", err)
+	cnt := blobCli.GetContainerReference(containerName)
+	options := storage.CreateContainerOptions{
+		Access: storage.ContainerAccessTypePrivate,
+	}
+	err := cnt.Create(&options)
+	if err != nil {
+		if accountName == storage.StorageEmulatorAccountName {
+			onErrorFail(err, "Create container failed: If you are running with the emulator credentials, plaase make sure you have started the storage emmulator. Press the Windows key and type Azure Storage to select and run it from the list of applications - then restart the sample")
 		}
-		return err
+		onErrorFail(err, "Create container failed")
 	}
 
-	printError(appendBlobOperations(&blobClient, containerName, appendBlobName))
-	printError(blockBlobOperations(&blobClient, containerName, blockBlobName))
-	printError(pageBlobOperations(&blobClient, containerName, pageBlobName))
+	if !*emulator {
+		err = appendBlobOperations(cnt, appendBlobName)
+		onErrorFail(err, "Append blob operations failed")
+	}
 
-	/*
-		fmt.Println("Delete container...")
-		if _, err = blobClient.DeleteContainerIfExists(containerName); err != nil {
-			return err
-		}
-	*/
-	return nil
+	err = blockBlobOperations(cnt, blockBlobName)
+	onErrorFail(err, "Block blob operations failed")
+
+	err = pageBlobOperations(cnt, pageBlobName)
+	onErrorFail(err, "Page blob operations failed")
+
+	err = printBlobList(cnt)
+	onErrorFail(err, "List blobs failed")
+
+	fmt.Print("Press enter to delete the blobs, container and local files created in this sample...")
+
+	var input string
+	fmt.Scanln(&input)
+
+	fmt.Println("Delete container...")
+	err = cnt.Delete(nil)
+	onErrorFail(err, "Delete container failed")
+
+	fmt.Println("Delete files...")
+	os.Remove(appendBlobFile)
+	os.Remove(blockBlobFile)
+	os.Remove(pageBlobFile)
+
+	fmt.Println("Done")
 }
 
 // appendBlobOperations performs simple append blob operations.
 // ExtraHeaders adds metadata to the blob (in this example, Content-Type).
 // For more information, please visit: https://msdn.microsoft.com/library/azure/ee691975.aspx
-func appendBlobOperations(blobClient *storage.BlobStorageClient, containerName, appendBlobName string) error {
+func appendBlobOperations(cnt storage.Container, appendBlobName string) error {
 	fmt.Println("Create an empty append blob...")
-	extraHeaders := map[string]string{
-		"Content-Type": "text/plain"}
-	if err := blobClient.PutAppendBlob(containerName, appendBlobName, extraHeaders); err != nil {
-		return err
+	b := cnt.GetBlobReference(appendBlobName)
+	b.Properties.ContentType = "text/plain"
+	err := b.PutAppendBlob(nil)
+	if err != nil {
+		return fmt.Errorf("put append blob failed: %v", err)
 	}
 
 	fmt.Println("Append a block to the blob...")
-	appendData := randomData(42) //Append blocks can have any length.
-	if err := blobClient.AppendBlock(containerName, appendBlobName, appendData, extraHeaders); err != nil {
-		return err
+	data := randomData(42) //Append blocks can have any length.
+	err = b.AppendBlock(data, nil)
+	if err != nil {
+		return fmt.Errorf("append block failed: %v", err)
 	}
 
-	printError(downloadBlob(blobClient, containerName, appendBlobName, "appendBlob.txt"))
-	printError(printBlobList(blobClient, containerName))
+	err = downloadBlob(b, appendBlobFile)
+	if err != nil {
+		return fmt.Errorf("download blob failed: %v", err)
+	}
 
-	/*
-		fmt.Println("Delete append blob...")
-		if _, err := blobClient.DeleteBlobIfExists(containerName, appendBlobName, nil); err != nil {
-			return err
-		}
-	*/
 	return nil
 }
 
@@ -96,130 +135,99 @@ func appendBlobOperations(blobClient *storage.BlobStorageClient, containerName, 
 // Blocks of the the block blob are uploaded with PutBlock function.
 // Once all the blocks are uploaded, PutBlockList is used to write/commit those blocks into the blob.
 // For more information, please visit: https://msdn.microsoft.com/library/azure/ee691964.aspx
-func blockBlobOperations(blobClient *storage.BlobStorageClient, containerName, blockBlobName string) error {
+func blockBlobOperations(cnt storage.Container, blockBlobName string) error {
 	fmt.Println("Create an empty block blob...")
-	if err := blobClient.CreateBlockBlob(containerName, blockBlobName); err != nil {
-		return err
-	}
-
-	fmt.Println("Upload a block...")
-	blockID := base64.StdEncoding.EncodeToString([]byte("foo"))
-	blockData := randomData(1984)
-	if err := blobClient.PutBlock(containerName, blockBlobName, blockID, blockData); err != nil {
-		return err
-	}
-
-	printError(printBlockList(blobClient, containerName, blockBlobName))
-
-	fmt.Println("Build uncommitted blocks list...")
-	blocksList, err := blobClient.GetBlockList(containerName, blockBlobName, storage.BlockListTypeUncommitted)
+	b := cnt.GetBlobReference(blockBlobName)
+	err := b.CreateBlockBlob(nil)
 	if err != nil {
-		return err
+		return fmt.Errorf("create block blob failed: %v", err)
 	}
-	uncommittedBlocksList := make([]storage.Block, len(blocksList.UncommittedBlocks))
-	for i := range blocksList.UncommittedBlocks {
-		uncommittedBlocksList[i].ID = blocksList.UncommittedBlocks[i].Name
+
+	fmt.Println("Put a block...")
+	blockID := base64.StdEncoding.EncodeToString([]byte("00000"))
+	data := randomData(1984)
+	err = b.PutBlock(blockID, data, nil)
+	if err != nil {
+		return fmt.Errorf("put block failed: %v", err)
+	}
+
+	err = printBlockList(b)
+	if err != nil {
+		return fmt.Errorf("get block list failed: %v", err)
+	}
+
+	fmt.Println("Get uncommitted blocks list...")
+	list, err := b.GetBlockList(storage.BlockListTypeUncommitted, nil)
+	if err != nil {
+		return fmt.Errorf("get block list failed: %v", err)
+	}
+	uncommittedBlocksList := make([]storage.Block, len(list.UncommittedBlocks))
+	for i := range list.UncommittedBlocks {
+		uncommittedBlocksList[i].ID = list.UncommittedBlocks[i].Name
 		uncommittedBlocksList[i].Status = storage.BlockStatusUncommitted
 	}
+
 	fmt.Println("Commit blocks...")
-	if err = blobClient.PutBlockList(containerName, blockBlobName, uncommittedBlocksList); err != nil {
-		return err
+	err = b.PutBlockList(uncommittedBlocksList, nil)
+	if err != nil {
+		return fmt.Errorf("put block list failed: %v", err)
 	}
 
-	printError(printBlockList(blobClient, containerName, blockBlobName))
-	printError(downloadBlob(blobClient, containerName, blockBlobName, "blockBlob.txt"))
-	printError(printBlobList(blobClient, containerName))
+	err = printBlockList(b)
+	if err != nil {
+		return fmt.Errorf("get block list failed: %v", err)
+	}
 
-	/*
-		fmt.Println("Delete block blob...")
-		if _, err = blobClient.DeleteBlobIfExists(containerName, blockBlobName, nil); err != nil {
-			return err
-		}
-	*/
+	err = downloadBlob(b, blockBlobFile)
+	if err != nil {
+		return fmt.Errorf("download blob failed: %v", err)
+	}
+
 	return nil
 }
 
 // pageBlobOperations performs simple page blob operations.
 // ExtraHeaders adds metadata to the blob (in this example, Content-Type).
 // For more information, please visit: https://msdn.microsoft.com/library/azure/ee691975.aspx
-func pageBlobOperations(blobClient *storage.BlobStorageClient, containerName, pageBlobName string) error {
+func pageBlobOperations(cnt storage.Container, pageBlobName string) error {
 	fmt.Println("Create an empty page blob...")
-	extraHeaders := map[string]string{
-		"Content-Type": "text/plain"}
+	b := cnt.GetBlobReference(pageBlobName)
+	b.Properties.ContentType = "text/plain"
 	// Page blobs' sizes must be multiples of 512.
-	if err := blobClient.PutPageBlob(containerName, pageBlobName, int64(512*5), extraHeaders); err != nil {
-		return err
+	b.Properties.ContentLength = int64(512 * 5)
+	err := b.PutPageBlob(nil)
+	if err != nil {
+		return fmt.Errorf("put page blob failed: %v", err)
 	}
 
 	fmt.Println("Writing in the page blob...")
 	pageLen := 512 * 3
-	pageData := randomData(pageLen)
-	// PutPage can update or clear data in the page blob, just change the writeType argument.
+	data := randomData(pageLen)
 	// The range for the chunks inside the blob must be aligned to 512 byte boundaries.
-	if err := blobClient.PutPage(containerName, pageBlobName, 0, int64(pageLen-1), storage.PageWriteTypeUpdate, pageData, extraHeaders); err != nil {
-		return err
+	br := storage.BlobRange{
+		End: uint64(pageLen - 1),
+	}
+	err = b.WriteRange(br, bytes.NewReader(data), nil)
+	if err != nil {
+		return fmt.Errorf("write range failed: %v", err)
 	}
 
 	fmt.Println("Get valid page ranges...")
-	pageRanges, err := blobClient.GetPageRanges(containerName, pageBlobName)
-	printError(err)
-	if err == nil {
-		fmt.Println("Valid page ranges:")
-		for _, pageRange := range pageRanges.PageList {
-			fmt.Printf("\tFrom page %v to page %v\n", pageRange.Start, pageRange.End)
-		}
-	}
-
-	printError(downloadBlob(blobClient, containerName, pageBlobName, "pageBlob.txt"))
-	printError(printBlobList(blobClient, containerName))
-
-	/*
-		fmt.Println("Delete page blob...")
-		if _, err = blobClient.DeleteBlobIfExists(containerName, pageBlobName, nil); err != nil {
-			return err
-		}
-	*/
-	return nil
-}
-
-// getCredentials returns a map containing the storage account name and key. It supports Azure Storage Emulator
-func getCredentials(env storageEnvironment) (map[string]string, error) {
-	credentials := map[string]string{}
-	switch env {
-	case emulator:
-		return map[string]string{
-			"AZURE_STORAGE_ACCOUNT_NAME": storage.StorageEmulatorAccountName,
-			"AZURE_STORAGE_ACCOUNT_KEY":  storage.StorageEmulatorAccountKey}, nil
-	case account:
-		credentials = map[string]string{
-			"AZURE_STORAGE_ACCOUNT_NAME": os.Getenv("AZURE_STORAGE_ACCOUNT_NAME"),
-			"AZURE_STORAGE_ACCOUNT_KEY":  os.Getenv("AZURE_STORAGE_ACCOUNT_KEY")}
-		if err := checkEnvVar(&credentials); err != nil {
-			return nil, err
-		}
-	}
-	return credentials, nil
-}
-
-// checkEnvVar checks if the environment variables are actually set.
-func checkEnvVar(envVars *map[string]string) error {
-	var missingVars []string
-	for name, v := range *envVars {
-		if v == "" {
-			missingVars = append(missingVars, name)
-		}
-	}
-	if len(missingVars) > 0 {
-		return fmt.Errorf("Missing environment variables %v", missingVars)
-	}
-	return nil
-}
-
-// printError prints non nil errors.
-func printError(err error) {
+	pageRanges, err := b.GetPageRanges(nil)
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
+		return fmt.Errorf("get page ranges failed: %v", err)
 	}
+	fmt.Println("Valid page ranges:")
+	for _, pageRange := range pageRanges.PageList {
+		fmt.Printf("\tFrom page %v to page %v\n", pageRange.Start, pageRange.End)
+	}
+
+	err = downloadBlob(b, pageBlobFile)
+	if err != nil {
+		return fmt.Errorf("download blob failed: %v", err)
+	}
+
+	return nil
 }
 
 // randomData returns a byte array with random bytes.
@@ -236,31 +244,41 @@ func randomData(strLen int) []byte {
 }
 
 // downloadBlob writes the blob's data into a local file.
-func downloadBlob(blobClient *storage.BlobStorageClient, containerName, blobName, fileName string) error {
-	fmt.Printf("Download blob data into '%v'...\n", fileName)
-	if _, err := os.Stat(fileName); err == nil {
-		return fmt.Errorf("File '%v' already exists", fileName)
+func downloadBlob(b storage.Blob, fileName string) error {
+	fmt.Printf("Download blob '%v' into '%v'...\n", b.Name, fileName)
+
+	_, err := os.Stat(fileName)
+	if err == nil {
+		return fmt.Errorf("file '%v' already exists", fileName)
 	}
-	readCloser, err := blobClient.GetBlob(containerName, blobName)
-	if err != nil {
-		return err
-	}
-	bytesRead, err := ioutil.ReadAll(readCloser)
+
+	readCloser, err := b.Get(nil)
 	defer readCloser.Close()
 	if err != nil {
-		return err
+		return fmt.Errorf("get blob failed: %v", err)
 	}
-	return ioutil.WriteFile(fileName, bytesRead, 0666)
+
+	bytesRead, err := ioutil.ReadAll(readCloser)
+	if err != nil {
+		return fmt.Errorf("read body failed: %v", err)
+	}
+
+	err = ioutil.WriteFile(fileName, bytesRead, 0666)
+	if err != nil {
+		return fmt.Errorf("write file failed: %v", err)
+	}
+
+	return nil
 }
 
 // printBlockList prints both committed and uncommitted blocks on a block blob.
-func printBlockList(blobClient *storage.BlobStorageClient, containerName, blockBlobName string) error {
+func printBlockList(b storage.Blob) error {
 	fmt.Println("Get block list...")
-	list, err := blobClient.GetBlockList(containerName, blockBlobName, storage.BlockListTypeAll)
+	list, err := b.GetBlockList(storage.BlockListTypeAll, nil)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("Block blob '%v' block list\n", blockBlobName)
+	fmt.Printf("Block blob '%v' block list\n", b.Name)
 	fmt.Println("\tCommitted Blocks' IDs")
 	for _, b := range list.CommittedBlocks {
 		fmt.Printf("\t\t%v\n", b.Name)
@@ -275,15 +293,34 @@ func printBlockList(blobClient *storage.BlobStorageClient, containerName, blockB
 // printBlobList prints all blobs' names in a container.
 // ListBlobsParameters can customize a blobs list.
 // For more information, please visit: https://godoc.org/github.com/Azure/azure-sdk-for-go/storage#ListBlobsParameters
-func printBlobList(blobClient *storage.BlobStorageClient, containerName string) error {
-	fmt.Printf("Get blob list from container '%v'...\n", containerName)
-	list, err := blobClient.ListBlobs(containerName, storage.ListBlobsParameters{})
+func printBlobList(cnt storage.Container) error {
+	fmt.Printf("Get blob list from container '%v'...\n", cnt.Name)
+	list, err := cnt.ListBlobs(storage.ListBlobsParameters{})
 	if err != nil {
 		return err
 	}
-	fmt.Printf("Blobs inside '%v' container:\n", containerName)
+	fmt.Printf("Blobs inside '%v' container:\n", cnt.Name)
 	for _, b := range list.Blobs {
 		fmt.Printf("\t%v\n", b.Name)
 	}
 	return nil
+}
+
+// getEnvVarOrExit returns the value of specified environment variable or terminates if it's not defined.
+func getEnvVarOrExit(varName string) string {
+	value := os.Getenv(varName)
+	if value == "" {
+		fmt.Printf("Missing environment variable %s\n", varName)
+		os.Exit(1)
+	}
+
+	return value
+}
+
+// onErrorFail prints a failure message and exits the program if err is not nil.
+func onErrorFail(err error, message string) {
+	if err != nil {
+		fmt.Printf("%s: %s\n", message, err)
+		os.Exit(1)
+	}
 }
